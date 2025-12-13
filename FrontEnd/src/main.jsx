49c82dc03b6a7af6cc181a3dec72718b50c0db61
Joseph <joseph.denorme@imt-atlantique.net>
2025-12-13 14:56:37 +0100

    Cleaning

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 import MyMapInit from './MyMap.jsx'
 import Detail from "./Detail.jsx";
-import Princ from "./Princ.jsx"
 
 
 
@@ -12,7 +10,6 @@ import Princ from "./Princ.jsx"
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      {/*<Route path="/" element={<Princ />} />*/}
       <Route path="/" element={<MyMapInit/>} />
       <Route path="/details/:id" element={<Detail />} />
     </Routes>
